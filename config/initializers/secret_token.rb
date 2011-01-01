86ef31449515be65e19c2db016b4ec1357598ea1
garfield5 <otvosdavid@gmail.com>
2011-01-01 21:38:54 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linda::Application.config.secret_token = 'ce76915eb17b0dfb6eee7c7878c4f01a4d397dde99ccbe518a3a29b5844cbeb0dae4e1d4360de4914f11c0b4ac2a7d774fa01389a85a17c89acd681995eab19b'
